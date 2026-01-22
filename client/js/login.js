4056851cbf6e2928d6c3e2ff66c4fe489b0115d4
harleenkaur.dhillon <harleendhillon25@gmail.com>
2026-01-22 10:30:36 +0000

    updated version

---
+++ client/js/login.js
@@ -0,0 +1,37 @@
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: form.get("email"),
+      password: form.get("password"),
+    })
+  };
+
+  const response = await fetch("remeber to put the link in", options);
+  const data = await response.json();
+
+  if (response.status === 200) {
+    window.location.assign("index.html");
+  } else {
+    alert("Login failed. Please try again.");
+  }
+});
+
+// Forgot password link
+document.getElementById("forgot-password").addEventListener("click", (e) => {
+  e.preventDefault();
+  alert(
+    "Your request has been sent to the administrator.\n\nThey will reset your password and email you shortly."
+  );
+});
+
+
+
