cb35fbb12c0a5275eed3e126c875ebd716fd4c9a
MVCRUST <max.crust7@gmail.com>
2026-01-23 11:40:46 +0000

    Made it so the client_log will truncate after every refresh as data is no longer needed

--- BACKEND/src/services/detectionService.js
+++ BACKEND/src/services/detectionService.js
@@ -4,7 +4,7 @@ require("dotenv").config();  // put this as the first line
 
 //Here will be the requires from the ingest.model (for the IPs) and I assume the iprep.model (to send the response)
 const iprepModel = require("../models/iprep.model.js")
-const {getDistinctIPsInWindow} = require("../models/ingest.model.js")
+const {getDistinctIPs, clearAllLogs } = require("../models/ingest.model.js")
 
 const API_KEY = process.env.ABUSE_KEY 
 //Need to put key in .env
@@ -49,17 +49,17 @@ async function callAbuseIPDB(ipAddress) {
 }
 
 //Don't think I need this function anymore, as was good for simply testing one, enrich RecentIPs is what it will be using.
-async function enrichIP(ipAddress) {
-    const record = await callAbuseIPDB(ipAddress);
+// async function enrichIP(ipAddress) {
+//     const record = await callAbuseIPDB(ipAddress);
 
-    const savedRow = await iprepModel.updateIPReputation(record);
+//     const savedRow = await iprepModel.updateIPReputation(record);
 
-    return savedRow;
-}
+//     return savedRow;
+// }
 
 //Get the IPs from the injest Model.
-async function enrichRecentIPs({ windowMinutes = 60 } = {}) {
-    const ips = await getDistinctIPsInWindow(windowMinutes) //Currently gets the last 60 minutes worth of IPs in table, can be changed.
+async function enrichRecentIPs() {
+    const ips = await getDistinctIPs() //Currently gets the last 60 minutes worth of IPs in table, can be changed.
 
     const results = []
 
@@ -81,24 +81,31 @@ async function enrichRecentIPs({ windowMinutes = 60 } = {}) {
             })
         }
     }
+    
+    //Once processed we can clear all the logs
+    try {
+        await clearAllLogs();
+        console.log("Client logs table cleared after refresh")
+    } catch (err) {
+        console.error("Failed to clear the client logs:", err.message)
+    }
+
     return results;
 }
-
 //Quick check to see if it works before I move onto iprepmodel, need terminal access to try this
-if (require.main === module) {
-    (async() => {
-        try {
-            console.log("This shows if it is actually running")
-            const result = await enrichIP("80.94.92.70") //Now testing the full flow as calling enrichIP
-            console.log(result)
-        } catch (err) {
-            console.log(err)
-        }
-    })(); // The () is so the function is actually called. This can be deleted/commented when actually pulling from other models.
-} 
+// if (require.main === module) {
+//     (async() => {
+//         try {
+//             console.log("This shows if it is actually running")
+//             const result = await enrichIP("80.94.92.70") //Now testing the full flow as calling enrichIP
+//             console.log(result)
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     })(); // The () is so the function is actually called. This can be deleted/commented when actually pulling from other models.
+// } 
 
 module.exports = {
     callAbuseIPDB,
-    enrichIP,
     enrichRecentIPs
 }
