d14087e938ae6030216ca26242022f89e3ba20a6
MVCRUST <max.crust7@gmail.com>
2026-01-27 09:09:10 +0000

    Updated app.js to connect the front end to localhost

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,9 +1,12 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const logger = require("./middleware/logger")
-const apiAuth = require("./middleware/api.auth.js")
-const clientAuth = require("./middleware/client.auth.js")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+const logger = require("./middleware/logger");
+const apiAuth = require("./middleware/api.auth.js");
+const clientAuth = require("./middleware/client.auth.js");
 
 // app.use(apiAuth);
 // app.use(clientAuth);
@@ -11,12 +14,29 @@ app.use(logger);
 app.use(cors());
 app.use(express.json());
 
-app.use('/clients', require('./routes/clients.routes'));
-app.use('/logs', require('./routes/ingest.routes'));
-app.use('/alerts', require('./routes/alerts.routes'));
-app.use('/ips', require('./routes/iprep.routes'));
+// Front end routes
+app.use(express.static(path.join(__dirname, "..", "..", "client")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "index.html"));
+});
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "login.html"));
+});
 
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "admin.html"));
+});
+
+app.get("/security", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "security.html"));
+});
+
+// API routes
+app.use("/clients", require("./routes/clients.routes"));
+app.use("/logs", require("./routes/ingest.routes"));
+app.use("/alerts", require("./routes/alerts.routes"));
+app.use("/ips", require("./routes/iprep.routes"));
 
 module.exports = app;
